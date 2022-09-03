b1847ddb53a570fe417a25faccc597363470df29
Will <103394466+OhDangBro@users.noreply.github.com>
2022-09-03 18:16:48 -0400

    challenge complete/will add more notes and do a final commit

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,5 +10,23 @@ const {
     deleteFriend
   } = require('../../controllers/user-controller');
 
+// Set up GET all and POST at /api/users
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
 
-  module.exports = router;
+// GET/PUT/DELETE   insomnia: /api/users/:id //
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+//ADD/DELETE Friend //
+router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
+
+module.exports = router;
