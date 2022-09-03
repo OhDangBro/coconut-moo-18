4e30af40d8fb6238e127cab3b15dc8c6fb1f9e37
Will <103394466+OhDangBro@users.noreply.github.com>
2022-09-03 13:20:49 -0400

    added data to thought-routes and controllers/thought controller/ to give running functionality to thought aspect

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -10,4 +10,27 @@ const {
   deleteReaction
 } = require('../../controllers/thought-controller');
 
+// GET and POST routes through: /api/thoughts
+router
+  .route('/')
+  .get(getAllThought)
+  .post(createThought);
+
+// GET/PUT/DELETE one routes through:  /api/thoughts/:id
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// Post at /api/thoughts/:thoughtId/reactions
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction);
+
+  router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
+
 module.exports = router;
